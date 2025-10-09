4457ec5fa01ad6c28cc4b46dcc62d9f11ba6489e
rhuanpk <dev@rhuanpk.net>
2025-10-09 16:37:30 -0300

    feat: update all mobile layout

---
+++ scripts/footer.js
@@ -0,0 +1,6 @@
+; (function () {
+	'use strict'
+
+	const footerYear = document.getElementById('year')
+	footerYear.textContent = new Date().getFullYear()
+})()
--- scripts/header.js
+++ scripts/header.js
@@ -2,15 +2,30 @@
 	'use strict'
 
 	const headerButton = document.querySelector('.header__button')
-	const headerNavigation = document.querySelector('.header__navigation')
+	// const headerNavigation = document.querySelector('.header__navigation')
+	const headerLinks = document.querySelectorAll('.header__link')
 
 	headerButton.addEventListener('click', event => {
 		event.stopPropagation()
 		headerNavigationToggle()
 	})
 
-	headerNavigation.addEventListener('click', event => {
-		event.stopPropagation()
-		headerNavigationToggle()
+	// headerNavigation.addEventListener('click', event => {
+	// 	event.stopPropagation()
+	// 	headerNavigationToggle()
+	// })
+
+	headerLinks.forEach(element => {
+		if (element.nodeName === 'A') {
+			element.addEventListener('click', event => {
+				event.stopPropagation()
+				headerNavigationToggle()
+			})
+			return
+		}
+
+		element.addEventListener('click', event => {
+			event.stopPropagation()
+		})
 	})
 })()
