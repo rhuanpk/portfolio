2f5d839cbac08c42a71747890dc401a08b460720
rhuanpk <dev@rhuanpk.net>
2025-10-10 12:01:19 -0300

    refact: all project

--- scripts/body.js
+++ scripts/body.js
@@ -1,31 +1,5 @@
 ; (function () {
 	'use strict'
 
-	const paddingBlock = parseVariable('--padding-block') // refact to get the header padding top directly
-
-	const header = document.querySelector('.header')
-	const headerNavigation = document.querySelector('.header__navigation')
-
-	document.addEventListener('scroll', () => {
-		const isMoreThanPadding = window.scrollY >= paddingBlock
-
-		const classHeaderFull = 'header--full'
-		const hasClassHeaderFull = header.classList.contains(classHeaderFull)
-
-		const classHeaderNavigationFull = 'header__navigation--full'
-		// const hasClassHeaderNavigationFull = header.classList.contains(classHeaderNavigationFull)
-
-		if (isMoreThanPadding && !hasClassHeaderFull) {
-			header.classList.add(classHeaderFull)
-			headerNavigation.classList.add(classHeaderNavigationFull)
-			return
-		}
-
-		if (!isMoreThanPadding && hasClassHeaderFull) {
-			header.classList.remove(classHeaderFull)
-			headerNavigation.classList.remove(classHeaderNavigationFull)
-		}
-	}, { passive: true })
-
 	document.body.addEventListener('click', headerNavigationClose)
 })()
--- scripts/header.js
+++ scripts/header.js
@@ -2,18 +2,12 @@
 	'use strict'
 
 	const headerButton = document.querySelector('.header__button')
-	// const headerNavigation = document.querySelector('.header__navigation')
 	const headerLinks = document.querySelectorAll('.header__link')
 
 	headerButton.addEventListener('click', event => {
 		event.stopPropagation()
 		headerNavigationToggle()
 	})
-
-	// headerNavigation.addEventListener('click', event => {
-	// 	event.stopPropagation()
-	// 	headerNavigationToggle()
-	// })
 
 	headerLinks.forEach(element => {
 		if (element.nodeName === 'A') {
